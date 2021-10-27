1c6d60320e440fb368c14e2538e41fcf2ff0055d
Mohammad Ummair <mohdummair007@gmail.com>
2021-10-27 12:55:58 +0530

    Delete mess_review directory

--- mess_review/home/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import student, admintable, dish,waste
-
-admin.site.register(student)
-admin.site.register(admintable)
-admin.site.register(dish)
-admin.site.register(waste)
-# Register your models here.
